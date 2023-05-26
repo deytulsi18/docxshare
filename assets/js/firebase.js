4e57883c9c70fb765817fba69dc0c996173560d3
Tulsi Dey <70888802+deytulsi18@users.noreply.github.com>
2023-05-26 13:26:06 +0530

    added firebase anonymous login

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,2 +1,14 @@
-
 firebase.initializeApp(firebaseConfig);
+
+firebase
+  .auth()
+  .signInAnonymously()
+  .then(() => {
+    console.log("Logged in as Anonymous!");
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  });
